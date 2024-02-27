5f9657c9ab601fa0198113e7a837d541396d7317
PrasadS25 <128603693+PrasadS25@users.noreply.github.com>
2024-02-27 22:52:54 +0530

    Time: 125 ms (26.37%), Space: 18.1 MB (42.09%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.py
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -1,27 +1,20 @@
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        #dfs
         maxarea = 0
         totalrows, totalcols = len(grid), len(grid[0])
-        
-        def bfs(r,c):
-            q = collections.deque()
-            q.append((r,c))
-            grid[r][c] = 0
-            directions = [[1,0],[0,1],[-1,0],[0,-1]]
+
+        def dfs(r,c):
+            if r not in range(totalrows) or c not in range(totalcols) or grid[r][c] == 0:
+                return 0
             area = 1
-            while q:
-                row,col = q.popleft()
-                for dr,dc in directions:
-                    nr,nc = row+dr, col+dc
-                    if (nr in range(0,totalrows)) and (nc in range(0,totalcols)) and (grid[nr][nc] == 1):
-                        area += 1
-                        grid[nr][nc] = 0
-                        q.append((nr,nc))
+            grid[r][c] = 0
+            for dr,dc in [[1,0],[0,1],[-1,0],[0,-1]]:
+                area += dfs(r+dr,c+dc)
             return area
 
         for r in range(totalrows):
             for c in range(totalcols):
                 if grid[r][c] == 1:
-                    maxarea = max(bfs(r,c),maxarea)
-
+                    maxarea = max(dfs(r,c),maxarea)
         return maxarea
